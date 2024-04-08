1fac4fc75da277c1d808447ec924ec4eb0d6ae55
orb7410 <orb7410@gmail.com>
2024-04-09 00:06:59 +0300

    add array of data fo jewelry boxes

--- src/pages/FindMyJewelry.js
+++ src/pages/FindMyJewelry.js
@@ -2,26 +2,27 @@ import React from 'react';
 import JewelryBox from '../JewelryBox';
 import './FindMyJewelry.css';
 
+const data = [
+    {
+        id: 1,
+        imgSrc: './assets/earring.jpeg',
+        txt: "silver earrings"
+    },
+    {
+        id: 2,
+        imgSrc: './assets/earring.jpeg',
+        txt: "gold earrings"
+    },
+    {
+        id: 3,
+        imgSrc: './assets/earring.jpeg',
+        txt: "rose gold earrings"
+    }
+]
+
 const FindMyJewelry = () => {
     return ( 
         <div id= "Jewelries">
-            <JewelryBox/>
-            <JewelryBox/>
-            <JewelryBox/>
-            <JewelryBox/>
-            <JewelryBox/>
-            <JewelryBox/>
-            <JewelryBox/>
-            <JewelryBox/>
-            <JewelryBox/>
-            <JewelryBox/>
-            <JewelryBox/>
-            <JewelryBox/>
-            <JewelryBox/>
-            <JewelryBox/>
-            <JewelryBox/>
-            <JewelryBox/>
-            <JewelryBox/>
             <JewelryBox/>
         </div>
         );
