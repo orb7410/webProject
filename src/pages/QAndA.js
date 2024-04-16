0f48e0eb1bd706c39f1f85fd87830cb1a2ad1100
orb7410 <orb7410@gmail.com>
2024-04-17 00:28:15 +0300

    add questions

--- src/pages/QAndA.js
+++ src/pages/QAndA.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import Question from '../Question';
 
-
+const questions = [
+    {
+        id: 1,
+        question: "what is my name?",
+        answer: "Avigil"
+    },
+    {
+        id: 2,
+        question: "what are the jewelries made of?",
+        answer: "silver"
+    },
+]
 
 const QAndA = () => {
     return (
-        <p className={"Q-and-a-text"}> what is this website?!!</p>
+        
+        <div className= "Q-and-a-text">
+            {questions.map(item => ( 
+            <Question key={questions.id} id={questions.id} question={item.question} answer={item.answer} />
+            ))}
+        </div>
     );
-    
 }
 export default QAndA;
 
