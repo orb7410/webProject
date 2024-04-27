7d1ee6e75f0cb80670c6413640af0cced01c287c
orb7410 <orb7410@gmail.com>
2024-04-28 00:28:03 +0300

    cosmetics

--- src/pages/JewelryDetails.js
+++ src/pages/JewelryDetails.js
@@ -3,6 +3,7 @@ import {useParams} from "react-router-dom";
 import './JewelryDetails.css';
 
 const img =  'https://lovediamonds.com/wp-content/uploads/2021/12/F400_55.jpg'
+const txt = "silver neckless"
 
 const JewelryDetails = () => {
     const {id} = useParams()
@@ -10,7 +11,7 @@ const JewelryDetails = () => {
         <div>
         <div className="container">
             <div className="pane1"> <img src={img} className="Jewelry_detail-img" alt="Jewelry" /></div>
-            <div className="pane2"> <h1>Jewelry details {id}</h1></div>
+            <div className="pane2"> <h1>{txt} id - {id}</h1></div>
         </div>
         <div className="pane3"> <h1>bottom {id}</h1></div>
         </div>
